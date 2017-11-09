c8742ab2a01acacc7696d04a472caf0e9d101d8c
Sirpoma Osku <s1601396@student.businesscollege.fi>
2017-11-09 08:44:14 +0200

    Getterit ja setterit Avatut classille

--- Joulukalenteri/src/Backend/Avatut.java
+++ Joulukalenteri/src/Backend/Avatut.java
@@ -10,5 +10,221 @@ package Backend;
  * @author s1601396
  */
 public class Avatut {
-    
+
+    private boolean luukku1 = false;
+    private boolean luukku2 = false;
+    private boolean luukku3 = false;
+    private boolean luukku4 = false;
+    private boolean luukku5 = false;
+    private boolean luukku6 = false;
+    private boolean luukku7 = false;
+    private boolean luukku8 = false;
+    private boolean luukku9 = false;
+    private boolean luukku10 = false;
+    private boolean luukku11 = false;
+    private boolean luukku12 = false;
+    private boolean luukku13 = false;
+    private boolean luukku14 = false;
+    private boolean luukku15 = false;
+    private boolean luukku16 = false;
+    private boolean luukku17 = false;
+    private boolean luukku18 = false;
+    private boolean luukku19 = false;
+    private boolean luukku20 = false;
+    private boolean luukku21 = false;
+    private boolean luukku22 = false;
+    private boolean luukku23 = false;
+    private boolean luukku24 = false;
+
+    public void setLuukku1(boolean luukku1) {
+        this.luukku1 = luukku1;
+    }
+
+    public void setLuukku2(boolean luukku2) {
+        this.luukku2 = luukku2;
+    }
+
+    public void setLuukku3(boolean luukku3) {
+        this.luukku3 = luukku3;
+    }
+
+    public void setLuukku4(boolean luukku4) {
+        this.luukku4 = luukku4;
+    }
+
+    public void setLuukku5(boolean luukku5) {
+        this.luukku5 = luukku5;
+    }
+
+    public void setLuukku6(boolean luukku6) {
+        this.luukku6 = luukku6;
+    }
+
+    public void setLuukku7(boolean luukku7) {
+        this.luukku7 = luukku7;
+    }
+
+    public void setLuukku8(boolean luukku8) {
+        this.luukku8 = luukku8;
+    }
+
+    public void setLuukku9(boolean luukku9) {
+        this.luukku9 = luukku9;
+    }
+
+    public void setLuukku10(boolean luukku10) {
+        this.luukku10 = luukku10;
+    }
+
+    public void setLuukku11(boolean luukku11) {
+        this.luukku11 = luukku11;
+    }
+
+    public void setLuukku12(boolean luukku12) {
+        this.luukku12 = luukku12;
+    }
+
+    public void setLuukku13(boolean luukku13) {
+        this.luukku13 = luukku13;
+    }
+
+    public void setLuukku14(boolean luukku14) {
+        this.luukku14 = luukku14;
+    }
+
+    public void setLuukku15(boolean luukku15) {
+        this.luukku15 = luukku15;
+    }
+
+    public void setLuukku16(boolean luukku16) {
+        this.luukku16 = luukku16;
+    }
+
+    public void setLuukku17(boolean luukku17) {
+        this.luukku17 = luukku17;
+    }
+
+    public void setLuukku18(boolean luukku18) {
+        this.luukku18 = luukku18;
+    }
+
+    public void setLuukku19(boolean luukku19) {
+        this.luukku19 = luukku19;
+    }
+
+    public void setLuukku20(boolean luukku20) {
+        this.luukku20 = luukku20;
+    }
+
+    public void setLuukku21(boolean luukku21) {
+        this.luukku21 = luukku21;
+    }
+
+    public void setLuukku22(boolean luukku22) {
+        this.luukku22 = luukku22;
+    }
+
+    public void setLuukku23(boolean luukku23) {
+        this.luukku23 = luukku23;
+    }
+
+    public void setLuukku24(boolean luukku24) {
+        this.luukku24 = luukku24;
+    }
+
+    public boolean isLuukku1() {
+        return luukku1;
+    }
+
+    public boolean isLuukku2() {
+        return luukku2;
+    }
+
+    public boolean isLuukku3() {
+        return luukku3;
+    }
+
+    public boolean isLuukku4() {
+        return luukku4;
+    }
+
+    public boolean isLuukku5() {
+        return luukku5;
+    }
+
+    public boolean isLuukku6() {
+        return luukku6;
+    }
+
+    public boolean isLuukku7() {
+        return luukku7;
+    }
+
+    public boolean isLuukku8() {
+        return luukku8;
+    }
+
+    public boolean isLuukku9() {
+        return luukku9;
+    }
+
+    public boolean isLuukku10() {
+        return luukku10;
+    }
+
+    public boolean isLuukku11() {
+        return luukku11;
+    }
+
+    public boolean isLuukku12() {
+        return luukku12;
+    }
+
+    public boolean isLuukku13() {
+        return luukku13;
+    }
+
+    public boolean isLuukku14() {
+        return luukku14;
+    }
+
+    public boolean isLuukku15() {
+        return luukku15;
+    }
+
+    public boolean isLuukku16() {
+        return luukku16;
+    }
+
+    public boolean isLuukku17() {
+        return luukku17;
+    }
+
+    public boolean isLuukku18() {
+        return luukku18;
+    }
+
+    public boolean isLuukku19() {
+        return luukku19;
+    }
+
+    public boolean isLuukku20() {
+        return luukku20;
+    }
+
+    public boolean isLuukku21() {
+        return luukku21;
+    }
+
+    public boolean isLuukku22() {
+        return luukku22;
+    }
+
+    public boolean isLuukku23() {
+        return luukku23;
+    }
+
+    public boolean isLuukku24() {
+        return luukku24;
+    }
 }
