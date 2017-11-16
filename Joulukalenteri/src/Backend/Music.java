3ae0e28fca55fcc3ab82a17258f61c3a6d8595e1
Sirpoma Osku <s1601396@student.businesscollege.fi>
2017-11-16 13:13:56 +0200

    -

--- Joulukalenteri/src/Backend/Music.java
+++ Joulukalenteri/src/Backend/Music.java
@@ -7,13 +7,7 @@ package Backend;
 
 import java.io.File;
 import java.io.IOException;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.*;
 
 public class Music {
 
