fe3ea68567b5aafd9b4d3c7860d356c04d5ce13e
infyleaf <32358643+infyleaf@users.noreply.github.com>
2017-11-24 10:25:08 +0200

    Added time.java class

---
+++ Joulukalenteri/src/Backend/Time.java
@@ -0,0 +1,139 @@
+package Backend;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Time {
+
+    private boolean luukku1 = false;
+    private boolean luukku2 = false;
+    private boolean luukku3 = false;
+    private boolean luukku4 = false;
+    private boolean luukku5 = false;
+    private boolean luukku6 = false;
+    private boolean luukku7 = false;
+    private boolean luukku8 = false;
+    private boolean luukku9 = false;
+    private boolean luukku10 = false;
+    private boolean luukku11 = false;
+    private boolean luukku12 = false;
+    private boolean luukku13 = false;
+    private boolean luukku14 = false;
+    private boolean luukku15 = false;
+    private boolean luukku16 = false;
+    private boolean luukku17 = false;
+    private boolean luukku18 = false;
+    private boolean luukku19 = false;
+    private boolean luukku20 = false;
+    private boolean luukku21 = false;
+    private boolean luukku22 = false;
+    private boolean luukku23 = false;
+    private boolean luukku24 = false;
+
+    public void main(String[] args) {
+
+        Avatut avaa = new Avatut();
+        Calendar pv1 = new GregorianCalendar(2017, Calendar.DECEMBER, 1);
+        Calendar pv2 = new GregorianCalendar(2017, Calendar.DECEMBER, 2);
+        Calendar pv3 = new GregorianCalendar(2017, Calendar.DECEMBER, 3);
+        Calendar pv4 = new GregorianCalendar(2017, Calendar.DECEMBER, 4);
+        Calendar pv5 = new GregorianCalendar(2017, Calendar.DECEMBER, 5);
+        Calendar pv6 = new GregorianCalendar(2017, Calendar.DECEMBER, 6);
+        Calendar pv7 = new GregorianCalendar(2017, Calendar.DECEMBER, 7);
+        Calendar pv8 = new GregorianCalendar(2017, Calendar.DECEMBER, 8);
+        Calendar pv9 = new GregorianCalendar(2017, Calendar.DECEMBER, 9);
+        Calendar pv10 = new GregorianCalendar(2017, Calendar.DECEMBER, 10);
+        Calendar pv11 = new GregorianCalendar(2017, Calendar.DECEMBER, 11);
+        Calendar pv12 = new GregorianCalendar(2017, Calendar.DECEMBER, 12);
+        Calendar pv13 = new GregorianCalendar(2017, Calendar.DECEMBER, 13);
+        Calendar pv14 = new GregorianCalendar(2017, Calendar.DECEMBER, 14);
+        Calendar pv15 = new GregorianCalendar(2017, Calendar.DECEMBER, 15);
+        Calendar pv16 = new GregorianCalendar(2017, Calendar.DECEMBER, 16);
+        Calendar pv17 = new GregorianCalendar(2017, Calendar.DECEMBER, 17);
+        Calendar pv18 = new GregorianCalendar(2017, Calendar.DECEMBER, 18);
+        Calendar pv19 = new GregorianCalendar(2017, Calendar.DECEMBER, 19);
+        Calendar pv20 = new GregorianCalendar(2017, Calendar.DECEMBER, 20);
+        Calendar pv21 = new GregorianCalendar(2017, Calendar.DECEMBER, 21);
+        Calendar pv22 = new GregorianCalendar(2017, Calendar.DECEMBER, 22);
+        Calendar pv23 = new GregorianCalendar(2017, Calendar.DECEMBER, 23);
+        Calendar pv24 = new GregorianCalendar(2017, Calendar.DECEMBER, 24);
+
+        Date now = new Date();
+
+        if ((now.after(pv1.getTime()) || now.equals(pv1.getTime()))) {
+  
+        }
+        if ((now.after(pv2.getTime()) || now.equals(pv2.getTime()))) {
+
+        }
+        if ((now.after(pv3.getTime()) || now.equals(pv3.getTime()))) {
+
+        }
+        if ((now.after(pv4.getTime()) || now.equals(pv4.getTime()))) {
+
+        
+        }
+        if ((now.after(pv5.getTime()) || now.equals(pv5.getTime()))) {
+
+        }
+        if ((now.after(pv6.getTime()) || now.equals(pv6.getTime()))) {
+
+        }
+        if ((now.after(pv7.getTime()) || now.equals(pv7.getTime()))) {
+  
+        }
+        if ((now.after(pv8.getTime()) || now.equals(pv8.getTime()))) {
+
+        }
+        if ((now.after(pv9.getTime()) || now.equals(pv9.getTime()))) {
+
+        }
+        if ((now.after(pv10.getTime()) || now.equals(pv10.getTime()))) {
+
+        }
+        if ((now.after(pv11.getTime()) || now.equals(pv11.getTime()))) {
+
+        }
+        if ((now.after(pv12.getTime()) || now.equals(pv12.getTime()))) {
+
+        }
+        if ((now.after(pv13.getTime()) || now.equals(pv13.getTime()))) {
+
+        }
+        if ((now.after(pv14.getTime()) || now.equals(pv14.getTime()))) {
+
+        }
+        if ((now.after(pv15.getTime()) || now.equals(pv15.getTime()))) {
+
+        }
+        if ((now.after(pv16.getTime()) || now.equals(pv16.getTime()))) {
+
+        }
+        if ((now.after(pv17.getTime()) || now.equals(pv17.getTime()))) {
+
+        }
+        if ((now.after(pv18.getTime()) || now.equals(pv18.getTime()))) {
+
+        }
+        if ((now.after(pv19.getTime()) || now.equals(pv19.getTime()))) {
+
+        }
+        if ((now.after(pv20.getTime()) || now.equals(pv20.getTime()))) {
+
+        }
+        if ((now.after(pv21.getTime()) || now.equals(pv21.getTime()))) {
+
+        }
+        if ((now.after(pv22.getTime()) || now.equals(pv22.getTime()))) {
+
+        }
+        if ((now.after(pv23.getTime()) || now.equals(pv23.getTime()))) {
+
+        }
+        if ((now.after(pv24.getTime()) || now.equals(pv24.getTime()))) {
+
+        }
+
+    }
+}
